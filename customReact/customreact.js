0eac8afc9d45628002aeb59878080ba5d07ab587
yash-sans2004 <nayan251198@gmail.com>
2023-12-14 03:37:25 +0530

    Create your own custom react

---
+++ customReact/customreact.js
@@ -0,0 +1,30 @@
+function customRender(reactElement, mainContainer){
+   /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute("href", reactElement.props.href)
+    domElement.setAttribute("target", reactElement.props.target)
+
+    mainContainer.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    mainContainer.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "Click me to visit again"
+}
+
+const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
