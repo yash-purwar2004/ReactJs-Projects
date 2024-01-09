c35cf1ab702184df2b6149dbab29504f774aa76e
yash-sans2004 <nayan251198@gmail.com>
2024-01-09 14:48:06 +0530

    Make a background changer on vite using react js.

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-200" style = {{backgroundColor: color}}>
+          <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+            <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-red-600" onClick={()=> setColor("red")}>
+                  Red
+              </button>
+
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-green-600" onClick={()=> setColor("green")}>
+                  Green
+              </button>
+
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-blue-600" onClick={()=> setColor("blue")}>
+                  Blue
+              </button>
+
+              <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg bg-yellow-600" onClick={()=> setColor("yellow")}>
+                  Yellow
+              </button>
+            </div>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
